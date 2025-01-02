3c539cb12b1524037a8f237fc28dc2e0deb58d61
Manjur9 <manjur.ali@metadesignsoftware.com>
2025-01-02 17:11:31 +0530

    "added changes"

---
+++ Arrays/removeDuplicatesSortedArray.js
@@ -0,0 +1,10 @@
+function removeDuplicates(nums) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] == nums[i + 1]) {
+            nums.splice(i, 1);
+            i--; //on removing do i-- and on adding do i++
+        }
+    }
+
+    return nums.length;
+};
