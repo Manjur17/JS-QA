77c791ecf7aa56e904c942461ccbd3a203b5e1e6
Manjur17 <skmanjuralli9@gmail.com>
2025-01-30 14:58:50 +0530

    "ddeep"

--- ProblemSolving/deepCopy.js
+++ ProblemSolving/deepCopy.js
@@ -9,10 +9,18 @@ function makeDeepCopy(obj) {
     let finalObj = Array.isArray(obj) ? [] : {};
 
     for (let key in obj) {
-        finalObj[key] = makeDeepCopy(obj[key]); // Recursively clone nested objects
+        finalObj[key] = makeDeepCopy(obj[key]); //we have to check if inner key's value is nested or not recursively
     }
 
     return finalObj;
+
+    // let keys = Object.keys(obj);
+
+    // for (let i = 0; i < keys.length; i++) {
+    //     const element = keys[i];
+    //     //console.log(obj.element); //why this gives undefined as element is variable and not a key
+    //     finalObj[element] = makeDeepCopy(obj[element]) //we have to check if inner key's value is nested or not recursively
+    // }
 }
 
 const ans = makeDeepCopy({ name: "Manjur", age: 25 });
